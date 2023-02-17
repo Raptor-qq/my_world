0a6cd2697a775a66744dfc0c9293ed6ed7ff8a9b
Raptor_qq <raptor13@mail.ru>
2023-02-17 17:45:37 +0500

    js operators

---
+++ web/2022/js-peremennie/js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+let eyeColor = 'blue',
+	userName = 'Вася',
+	user = userName;
+console.log(user);
+
+const BLOCK_SIZE = 14 + 50;
+console.log(BLOCK_SIZE);
+
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+let x;
+x = 5 + 8;
+console.log(`Результат сложения: ${x}`);
